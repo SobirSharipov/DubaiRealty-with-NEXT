921e5c6460e9c4b19bd80cecc2063f88d301b40e
Пользователь <sobirjonsharipov590@gmail.com>
2025-08-11 16:00:26 +0500

    DubaiRealty with NEXT

--- src/app/Blog/page.jsx
+++ src/app/Blog/page.jsx
@@ -129,9 +129,16 @@ const Blog = () => {
             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
           </svg>
         </div>
-        <Button type="primary" onClick={showModal}>
+        <div className='lg:block hidden'>
+        <Button type="primary" onClick={showModal} >
           ADD PRODUCTS
         </Button>
+        </div>
+        <div className='lg:hidden'>
+        <Button type="primary" onClick={showModal} >
+          +
+        </Button>
+        </div>
       </div>
 
 
